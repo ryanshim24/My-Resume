4e26d7ba9528544821d9f7697800fae885131ee2
ryanshim24 <ryanshim24@yahoo.com>
2014-11-16 01:13:52 -0800

    My First and Final commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    app = express();
+    
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/assets'));
+
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+
+var server = app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
